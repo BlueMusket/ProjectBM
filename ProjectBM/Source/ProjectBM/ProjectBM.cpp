7c88a9419fc7162724c592bd9b52d8d42995645f
leehoseo <leehoseo@game.hoseo.edu>
2023-12-27 23:35:55 +0900

    [이호서] 블루머스킷 프로젝트 Kick Off

---
+++ ProjectBM/Source/ProjectBM/ProjectBM.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ProjectBM.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ProjectBM, "ProjectBM" );
