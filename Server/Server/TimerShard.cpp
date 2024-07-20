1cb1312d13ec3482d286dddbf03dfeb79fd8a396
LAPTOP-NJ38A8SC\User <leehoseo@game.hoseo.edu>
2024-07-20 22:55:28 +0900

    노트북 테스트

--- Server/Server/TimerShard.cpp
+++ Server/Server/TimerShard.cpp
@@ -1,2 +1,7 @@
 #include "pch.h"
 #include "TimerShard.h"
+
+bool CTimerShard::Setup()
+{
+    return true;
+}
--- Server/Server/TimerShard.h
+++ Server/Server/TimerShard.h
@@ -1,5 +1,13 @@
 #pragma once
-class CTimerShard
+#include "Base/Singleton.h"
+
+class CAsyncTimerEvent;
+class CTimerShard : public CSingleton<CTimerShard>
 {
+public:
+	virtual bool Setup() override;
+
+private:
+	tbb::concurrent_queue<CAsyncTimerEvent*> m_EventShard;
 };
 
